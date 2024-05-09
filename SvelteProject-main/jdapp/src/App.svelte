e8a90953fd35ccee9b549800d0a4864d4748c58f
Juan barrezueta <Juanron8232@gmail.com>
2024-05-08 20:25:11 -0400

    New small changes

---
+++ SvelteProject-main/jdapp/src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	export let name: string;
+	let beltColor = 'black';
+	const handleClick = () => {
+		beltColor = 'Orange'
+	};
+
+	const handleInput = (e) => {
+		beltColor = e.target.value
+	};
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p style="color:{beltColor}"> {beltColor} </p>
+	<button on:click={handleClick}>update belt color</button>
+	<input type="text" on:input={handleInput} value={beltColor}>
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
